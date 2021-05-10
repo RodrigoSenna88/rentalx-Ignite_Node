32b50b0e1350f00e55673f9c14555e52663c6c73
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-05-10 16:46:36 -0300

    refatorando arquivos de repositório, usecase, index e controller referente a CReateCategory. Método agora salvando no banco de dados

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,14 +1,14 @@
-import { Category } from "../model/Category";
+import { Category } from "../entities/Category";
 
-interface ICreateDTO {
+interface ICreateCategoryDTO {
   name: string;
   description: string;
 }
 
 interface ICategoriesRepository {
-  findByName(name: string): Category;
-  list(): Category[];
-  create({ name, description }: ICreateDTO): void;
+  findByName(name: string): Promise<Category>;
+  list(): Promise<Category[]>;
+  create({ name, description }: ICreateCategoryDTO): Promise<void>;
 }
 
-export { ICategoriesRepository, ICreateDTO };
+export { ICategoriesRepository, ICreateCategoryDTO };
