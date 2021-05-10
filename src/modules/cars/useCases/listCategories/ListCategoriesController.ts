7b9048829f2ce1271882a5fd161992e42e165f93
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-05-10 18:56:42 -0300

    refatorando usecase, controller de List e Import de Categorias e de CreateSpecifications. Configuração de injeção de dependências utilizando TSyringe realizada.

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handle(request: Request, response: Response) {
-    const all = this.listCategoriesUseCase.execute();
+    const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
   }
